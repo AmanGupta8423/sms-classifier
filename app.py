f820b69814e2c961c5894405ac6ff5f98f280c3f
Aman Gupta <rv73757@gmail.com>
2026-06-11 14:43:55 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import nltk
+from nltk.corpus import stopwords
+import string
+import pickle 
+from nltk.stem.porter import PorterStemmer
+import sklearn
+
+ps = PorterStemmer()
+tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
+
+st.title('Sms Spam Classifier')
+
+def transform_text(text):
+    text = text.lower()
+
+    text = nltk.word_tokenize(text)
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:] 
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+input_sms = st.text_area("Enter the message")
+# st.write("The current movie title is", title)
+
+if st.button('Predict'):
+
+    #1 Pre processing the input
+    transformed_sms = transform_text(input_sms)
+
+    #2 Vectorize
+    vector_input = tfidf.transform([transformed_sms]) 
+
+    #3 Predict 
+    prediction = model.predict(vector_input)[0]
+
+
+    #4 Display
+    if prediction == 1:
+        st.header("Spam")
+
+    else:
+        st.header("Not Spam")
